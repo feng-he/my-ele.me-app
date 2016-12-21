f791b62fd891accf0578bbe07560af1fa2925248
hefeng <missky_hf@163.com>
2016-12-22 01:02:36 +0800

    component: header

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+  .goods
+    display: flex
+    position: absolute
+    top: 175px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background-color: #f3f5f7
+    .foods-wrapper
+      flex: 1
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template>
+  hey, seller
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+
+</style>
